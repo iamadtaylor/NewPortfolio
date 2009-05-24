8a6713e2202bdfd6aa9a4d0c411883e254754be0
Adam Taylor <github@adtaylor.otherninbox.com>
2009-05-24 10:20:15 +0100

    Testing the grid

--- index.php
+++ index.php
@@ -7,9 +7,152 @@
 
 	<title>I am Ad Taylor</title>
 	
+	<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+	<link rel="stylesheet" href="css/typography.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+	<link rel="stylesheet" href="css/images.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+	<link rel="stylesheet" href="css/home.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+	<link rel="stylesheet" href="css/18px.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+	
+
+	
 </head>
 
 <body>
+<div class="one">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="two">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="three">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="four">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="five">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="six">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="seven">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="eight">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="nine">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="ten">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="eleven">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="twelve">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="thirteen">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="fourteen">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+<div class="fifteen">&nbsp;</div>
+
+							<div class="clear">&nbsp;</div>
+							
+
+<!-- PUSH LEFTS -->
+							<div class="one">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="two pushLeftOne">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="three pushLeftTwo">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="four pushLeftThree">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="five pushLeftFour">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="six pushLeftFive">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="seven pushLeftSix">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="eight pushLeftSeven">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="nine pushLeftEight">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="ten pushLeftNine">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="eleven pushLeftTen">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="twelve pushLeftEleven">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="thirteen pushLeftTwelve">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="fourteen pushLeftThirteen">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+							<div class="fifteen pushLeftFourteen">&nbsp;</div>
+
+														<div class="clear">&nbsp;</div>
+
+<!-- PUSH RIGHTS -->
+
+
+
+	<div class="two pushRightThree">&nbsp;</div>
+
+
+	<div class="three pushRightTwo">&nbsp;</div>
+
+								<div class="clear">&nbsp;</div>
+
+	<div class="four pushLeftThree">&nbsp;</div>
+
 
 
 </body>
